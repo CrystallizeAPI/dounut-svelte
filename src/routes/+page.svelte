9d1998cc10c73b140284b063382c73d15de244a5
Bård Farstad <bard.farstad@gmail.com>
2023-10-26 16:31:55 +0200

    Initial try for grid rendering.. Not successful.

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,21 +11,28 @@
 
 <div class="py-20">
     <div
-      class="bg-background5 z-0 absolute left-0 right-0 bottom-0 rounded-tl-full rounded-tr-full"></div>
-    {#each $FrontPageGrid.data.grid.rows as row}
-        <div>
-            {#each row.columns as col}
+        class="bg-background5 z-0 absolute left-0 right-0 bottom-0 rounded-tl-full rounded-tr-full"></div>
+        <div style="display: grid; grid-template-columns: 1fr 1fr 1fr; max-width: 800px">
+            {#each $FrontPageGrid.data.grid.rows as row}
                 <div>
-                    <h2 class="text-2xl font-bold"><a href="{col.item.path}">{col.item.name}</a></h2>
-                    <span>{col.item.price}</span>
-                    <img width="300px" alt={col.item.defaultVariant.firstImage.altText} src={col.item.defaultVariant.firstImage.url} />
-                    {#if col.item.topics}
-                        {#each col.item.topics as topic}
-                            <div>{topic.name}</div>
-                        {/each}
-                    {/if}
+                    {#each row.columns as col}
+                        <div style="grid-column: span {col.layout.colspan};">
+                            colspan: {col.layout.colspan}
+                            <h2 class="text-2xl font-bold"><a href="{col.item.path}">{col.item.name}</a></h2>
+                            <span>{col.item.price}</span>
+                            <img width="300px" alt={col.item.defaultVariant.firstImage.altText} src={col.item.defaultVariant.firstImage.url} />
+                            {#if col.item.topics}
+                                {#each col.item.topics as topic}
+                                    <div>{topic.name}</div>
+                                {/each}
+                            {/if}
+                        </div>
+                    {/each}
                 </div>
             {/each}
         </div>
-    {/each}
 </div>
+
+<pre>
+    {JSON.stringify($FrontPageGrid, null, 2)}
+</pre>
