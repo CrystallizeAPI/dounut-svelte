e2989eab6d5c11260ecc29081047a7b49c5554b8
Bård Farstad <bf@Bards-Mac-mini.local>
2023-10-26 15:13:41 +0200

    Added basic styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,23 +9,28 @@
     $: FrontPageGrid, console.log(FrontPageGrid);
 </script>
 
-
-{$FrontPageGrid.data.grid.name}
-
-{#each $FrontPageGrid.data.grid.rows as row}
-    <div>
-    row
-        {#each row.columns as col}
-            <div>
-                <h2 class="text-2xl font-bold"><a href="{col.item.path}">{col.item.name}</a></h2>
-                <span>{col.item.price}</span>
-                <img width="300px" alt={col.item.defaultVariant.firstImage.altText} src={col.item.defaultVariant.firstImage.url} />
-                {#if col.item.topics}
-                    {#each col.item.topics as topic}
-                        <div>{topic.name}</div>
-                    {/each}
-                {/if}
-            </div>
-        {/each}
-    </div>
-{/each}
+<div class="py-20">
+    <div
+      class="bg-background5 z-0 absolute left-0 right-0 bottom-0 rounded-tl-full rounded-tr-full"
+      style={{
+        top: "45%",
+        zIndex: "-1",
+      }}
+    ></div>
+    {#each $FrontPageGrid.data.grid.rows as row}
+        <div>
+            {#each row.columns as col}
+                <div>
+                    <h2 class="text-2xl font-bold"><a href="{col.item.path}">{col.item.name}</a></h2>
+                    <span>{col.item.price}</span>
+                    <img width="300px" alt={col.item.defaultVariant.firstImage.altText} src={col.item.defaultVariant.firstImage.url} />
+                    {#if col.item.topics}
+                        {#each col.item.topics as topic}
+                            <div>{topic.name}</div>
+                        {/each}
+                    {/if}
+                </div>
+            {/each}
+        </div>
+    {/each}
+</div>
--- src/routes/shop/[path]/+page.svelte
+++ src/routes/shop/[path]/+page.svelte
@@ -11,53 +11,80 @@
 </script>
 
 <div>
-    <h1 class="text-2xl font-bold">{product.name}</h1>
+    <div
+        class="bg-background5 z-0 absolute left-0 right-0 rounded-full"
+        style={{
+          bottom: "10%",
+          top: "20%",
+          zIndex: "-1",
+        }}
+      ></div>
 
-    <div class="text-xl font-bold">$ {product.defaultVariant.price}</div>
-
-    {@html product.brief.content.html}
-
-    <img width="300px" alt={product.defaultVariant.firstImage.altText} src={product.defaultVariant.firstImage.url} />
-
-    {#if product.topics}
-        {#each product.topics as topic}
-            <div>{topic.name}</div>
-        {/each}
-    {/if}
-
-    <div>
-        {#if product.body.content}
-            {#each product.body.content.paragraphs as paragraph}
-                <h2 class="text-xl font-bold">{paragraph.title?.text}</h2>
-                <div>{@html paragraph.body.html}</div>
-                
-                {#if paragraph.images}
-                    {#each paragraph.images as image}
-                        <div>{image.url}</div>
-                        <img width="200px" alt={image.altText} src={image.url} />
+      <div class="py-5">
+        <div class="flex lg:flex-row flex-col items-center mt-10 mb-5">
+          <div class="flex flex-col text-text w-6/12">
+            <h1 class="font-extrabold text-5xl mb-3">{product.name}</h1>
+            {@html product.brief.content.html}
+          </div>
+          <img width="400px" class="w-full rounded-sm" alt={product.defaultVariant.firstImage.altText} src={product.defaultVariant.firstImage.url} />
+        </div>
+        <div class="flex z-10 justify-between lg:w-5/12 w-8/12 mx-auto bg-white p-5 text-text rounded-xl">
+        <div>
+          <p class="font-semibold text-sm">Total price</p>
+          <p class="font-bold text-lg">${product.defaultVariant.price}</p>
+        </div>
+        <button          
+          class="bg-background2 px-4 rounded-xl"
+        >
+          Add to Cart
+        </button>
+      </div>
+      <div>
+        <div class="flex flex-col gap-3 my-10 lg:w-9/12 w-full mx-auto z-10"></div>
+            <div class="flex flex-col justify-between">
+                <div class="my-3 text-text md:px-20">
+                {#if product.body.content}
+                    {#each product.body.content.paragraphs as paragraph}
+                        <h2 class="font-semibold text-2xl mb-4">{paragraph.title?.text}</h2>
+                        <div>{@html paragraph.body.html}</div>
+                        
+                        {#if paragraph.images}
+                        <div class="my-5">
+                            {#each paragraph.images as image}
+                                <img width="200px" class="rounded-xl overflow-hidden" alt={image.altText} src={image.url} />
+                            {/each}
+                        </div>
+                        {/if}        
                     {/each}
-               {/if}
-
-            {/each}
-        {/if}
-    </div>
-
-
-    <div>
-        {#if product.nutrition.content}
-            {#each product.nutrition.content.sections as section}
-                <h2 class="text-xl font-bold">{section.title}</h2>
-                {#each section.properties as prop}
-                <div>{prop.key}</div>
-                <div>{prop.value}</div>
-                {/each}
-            {/each}
-        {/if}
-
+                {/if}
+                </div>
+            </div>
+            <div class="flex lg:flex-row flex-col justify-between text-text my-20">
+                {#if product.nutrition.content}
+                    {#each product.nutrition.content.sections as section}
+                        <div>
+                            <h3 class="font-bold text-2xl py-2">{section.title}</h3>
+                            <p class="italic">per 50 g</p>
+                        </div>
+                        <div class="lg:w-7/12 w-full">
+                        {#each section.properties as prop}
+                        <div class="flex justify-between my-3 even:bg-grey px-5 py-2">
+                            <p>{prop.key}</p>
+                            <p>{prop.value}</p>
+                        </div>
+                        {/each}
+                        </div>
+                    {/each}
+                {/if}        
+            </div>
+            <p class="text-text mb-4 font-semibold">Related do(u)nuts</p>
+      </div>
+        RELATED PRODUCTS HERE
+        
     </div>
 </div>
 
-<hr />
+    <hr />
 <pre>
     {JSON.stringify($ProductData, null, 2)}
 </pre>
